3d539913bb810b8b7a78659345cb795b0fd103f1
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-03-01 20:57:35 +0600

    get search value

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+const searchButton = () =>{
+    // ger search text
+    const getSearchText = document.getElementById('search-text');
+    const searchText = getSearchText.value;
+    getSearchText.value = '';
+    console.log(searchText)
+    if(searchText ==""){
+      
+    }
+}
