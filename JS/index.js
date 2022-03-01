af5fbf3561cb45cc4b1e02e0b04d90d9ae183a6b
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-03-01 21:08:33 +0600

    load data

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,5 @@
+// get error id
+const error = document.getElementById('error')
 const searchButton = () =>{
     // ger search text
     const getSearchText = document.getElementById('search-text');
@@ -5,6 +7,12 @@ const searchButton = () =>{
     getSearchText.value = '';
     console.log(searchText)
     if(searchText ==""){
-      
+      error.innerText='Please give me search value'
+    }
+    // load data
+    else{
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+        .then(response => response.json())
+        .then(data => console.log(data.data))
     }
 }
